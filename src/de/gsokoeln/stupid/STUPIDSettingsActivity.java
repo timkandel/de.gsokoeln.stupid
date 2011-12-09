1cebaa1d14ac5a8a7940e50aa862286cf96a9095
Tim Kandel <timkandel@googlemail.com>
2011-12-09 09:05:39 +0100

    -

--- src/de/gsokoeln/stupid/STUPIDSettingsActivity.java
+++ src/de/gsokoeln/stupid/STUPIDSettingsActivity.java
@@ -11,11 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 

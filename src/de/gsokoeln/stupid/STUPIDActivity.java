a4daf1954c8a25dfb41eff7660c1e3821ba757e5
Tim Kandel <timkandel@googlemail.com>
2011-12-09 12:46:35 +0100

    - Kommentierung der STUPIDActivity

--- src/de/gsokoeln/stupid/STUPIDActivity.java
+++ src/de/gsokoeln/stupid/STUPIDActivity.java
@@ -30,12 +30,13 @@ public class STUPIDActivity extends Activity {
 	List<Calendar> startTimes = new ArrayList<Calendar>();
 	
 	/**
-	 * 
+	 * Generelle Initialisierung der Activity
 	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		// Einstellungsseite bei Klick auf "Einstellungen"-Button anzeigen
 		setContentView(R.layout.main);
 		Button preferencesButton = (Button)findViewById(R.id.preferencesButton);
         preferencesButton.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +46,7 @@ public class STUPIDActivity extends Activity {
             }
         });
         
+        // Daten bei Klick auf "Neu laden"-Button neu laden
         Button reloadButton = (Button)findViewById(R.id.reloadButton);
         reloadButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -54,18 +56,21 @@ public class STUPIDActivity extends Activity {
 	}
 	
 	/**
-	 * 
+	 * Ausführen des Klassencodes
 	 */
 	@Override
 	public void onStart() {
 		super.onStart();
 		
+		// Zeige Einstellungsseite wenn keine Klasse gewählt ist
 		if (0 == Util.getSchoolClass(this).length()) {
 			setContentView(R.layout.settings);
 			
 			Intent myIntent = new Intent(this, STUPIDSettingsActivity.class);
-            startActivityForResult(myIntent, 0);	
-		} else {
+            startActivityForResult(myIntent, 0);
+		} 
+		// Überprüfe ob Internetverbindung vorhanden ist und zeige dann Stundenplan
+		else {
 			if (!Util.isOnline(this)) {
 				Toast.makeText(this, "Kein Internet", Toast.LENGTH_LONG).show();
 			} else {
@@ -75,17 +80,19 @@ public class STUPIDActivity extends Activity {
 	}
 	
 	/**
+	 * Rückgabe der Activity (Hilfsmethode für Thread)
 	 * 
-	 * @return
+	 * @return Activity
 	 */
 	private Activity getActivity() {
 		return this;
 	}
 	
 	/**
-	 * 
+	 * GUI sperren und Laden / Ausführen in die Wege leiten
 	 */
 	private void displaySchedule() {
+		// Hole Buttons und ProgressBar / Buttons sperren und ProgressBar anzeigen
 		Button reloadButton = (Button)findViewById(R.id.reloadButton);
 		Button preferencesButton = (Button)findViewById(R.id.preferencesButton);
 		ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar);
@@ -94,13 +101,13 @@ public class STUPIDActivity extends Activity {
 		preferencesButton.setClickable(false);
 		Util.showProgressBar(progressBar);
 		
+		// Generiere Zeiten und lade Thread zum asynchronen Ausführen des Ladens / Auswertens der Daten
 		createdHardcodedPeriodStartTimes();
-		
 		new DisplayScheduleTask().execute();
 	}
 
 	/**
-	 * 
+	 * Generieren der Schulzeiten
 	 */
 	private void createdHardcodedPeriodStartTimes() {
 		Calendar today = new GregorianCalendar();
@@ -125,11 +132,13 @@ public class STUPIDActivity extends Activity {
 	}
 
 	/**
+	 * Auswerten der anzuzeigenden Stunden
 	 * 
-	 * @param lessonInformation
-	 * @param currentPeriod
+	 * @param lessonInformation Liste der anzuzeigenden Stunden
+	 * @param currentPeriod Nummer der aktuellen Stunde
 	 */
 	private void printLessonInformation(List<String> lessonInformation, int currentPeriod) {
+		// Hole Buttons und ProgressBar / Buttons freigeben und ProgressBar verstecken 
 		Button reloadButton = (Button)findViewById(R.id.reloadButton);
 		Button preferencesButton = (Button)findViewById(R.id.preferencesButton);
 		ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar);
@@ -138,6 +147,7 @@ public class STUPIDActivity extends Activity {
 		preferencesButton.setClickable(true);
 		Util.hideProgressBar(progressBar);
 	    
+		// Wenn Stunden vorhanden sind
 	    if (0 < lessonInformation.size()) {
 	    	String currentRoom = "---";
     		String currentTeacher = "---";
@@ -151,6 +161,7 @@ public class STUPIDActivity extends Activity {
     		String nextSubject = "---";
     		boolean nextLessonIsChanged = false;
 	    	
+    		// Wenn die aktuelle Stunde an erster Position ist
 	    	if (lessonInformation.get(0).equals(new Integer(currentPeriod).toString())) {
 	    		currentRoom = lessonInformation.get(1);
 	    		currentTeacher = lessonInformation.get(2);
@@ -159,6 +170,7 @@ public class STUPIDActivity extends Activity {
 	    		if (lessonInformation.get(4).equals("2"))
 	    			currentLessonIsChanged = true;
 	    		
+	    		// Wenn es eine weitere Stunde gibt
 	    		if (lessonInformation.size() > 5) {
 	    			nextRoom = lessonInformation.get(6);
 		    		nextTeacher = lessonInformation.get(7);
@@ -169,7 +181,9 @@ public class STUPIDActivity extends Activity {
 	    		} else {
 	    			hideNextLesson();
 	    		}
-	    	} else {
+	    	} 
+	    	// Wenn aktuelle Stunde an 2ter Stelle ist
+	    	else {
 	    		currentRoom = lessonInformation.get(6);
 	    		currentTeacher = lessonInformation.get(7);
 	    		currentPeriodNumber = lessonInformation.get(5);
@@ -185,6 +199,7 @@ public class STUPIDActivity extends Activity {
 	    			nextLessonIsChanged = true;
 	    	}
 	    	
+	    	// Stunden ausgeben
 	    	printCurrentLesson(currentRoom, currentTeacher, currentPeriodNumber, currentSubject, currentLessonIsChanged);
 	    	printNextLesson(nextRoom, nextTeacher, nextPeriodNumber, nextSubject, nextLessonIsChanged);
 	    } else {
@@ -193,7 +208,7 @@ public class STUPIDActivity extends Activity {
 	}
 	
 	/**
-	 * 
+	 * versteckt die aktuelle Stunde, wenn kein Unterricht ist 
 	 */
 	private void hideCurrentLesson() {
 		TextView currentRoomTextView = (TextView) findViewById(R.id.currentRoomTextView);
@@ -212,7 +227,7 @@ public class STUPIDActivity extends Activity {
 	}
 	
 	/**
-	 * 
+	 * versteckt die nächste Stunde, wenn kein Unterricht ist
 	 */
 	private void hideNextLesson() {
 		TextView spacerTextView = (TextView) findViewById(R.id.spacerTextView);
@@ -229,11 +244,12 @@ public class STUPIDActivity extends Activity {
 	}
 	
 	/**
+	 * Ausgeben der aktuellen Stunde
 	 * 
-	 * @param room
-	 * @param teacher
-	 * @param period
-	 * @param subject
+	 * @param room Raumkürzel
+	 * @param teacher Lehrerkürzel
+	 * @param period Nummer der Stunde
+	 * @param subject Fachkürzel
 	 */
 	private void printCurrentLesson(String room, String teacher, String period, String subject, boolean lessonIsChanged) {
 		TextView currentRoomTextView = (TextView) findViewById(R.id.currentRoomTextView);
@@ -264,11 +280,12 @@ public class STUPIDActivity extends Activity {
 	}
 	
 	/**
+	 * Ausgeben der nächsten Stunde
 	 * 
-	 * @param room
-	 * @param teacher
-	 * @param period
-	 * @param subject
+	 * @param room Raumkürzel
+	 * @param teacher Lehrerkürzel
+	 * @param period Nummer der Stunde
+	 * @param subject Fachkürzel
 	 */
 	private void printNextLesson(String room, String teacher, String period, String subject, boolean lessonIsChanged) {
 		TextView spacerTextView = (TextView)findViewById(R.id.spacerTextView);
@@ -302,13 +319,14 @@ public class STUPIDActivity extends Activity {
 	}
 	
 	/**
+	 * Auswerten der lesson.txt auf anzuzeigende Stunden
 	 * 
-	 * @param currentDay
-	 * @param currentWeek
-	 * @param completeSchedule
-	 * @param currentPeriod
-	 * @param className
-	 * @return
+	 * @param currentDay Nummer des aktuellen Tages
+	 * @param currentWeek Kalenderwoche
+	 * @param completeSchedule Gesamter Stundenplan
+	 * @param currentPeriod Nummer der aktuellen Stunde
+	 * @param className Klassenkürzel
+	 * @return anzuzeigende Stunden
 	 */
 	private List<String> getLessonInformation(int currentDay, int currentWeek, List<String[]> completeSchedule, int currentPeriod, String className) {
 		List<String> lessonInformation = new ArrayList<String>();
@@ -328,7 +346,7 @@ public class STUPIDActivity extends Activity {
 								lessonInformation.add(currentSchedule[4]); 		// Welcher Raum
 								lessonInformation.add(currentSchedule[0]); 		// Welcher Lehrer
 								lessonInformation.add(currentSchedule[3]);		// Welches Fach
-								lessonInformation.add(currentSchedule[6]);		// Stundenplanänderung ?
+								lessonInformation.add(currentSchedule[6]);		// Stundenplanänderung
 							}
 						}
 					}
@@ -340,20 +358,23 @@ public class STUPIDActivity extends Activity {
 	}
 	
 	/**
+	 * Überprüfen welche Schulstunde gerade läuft bzw. als nächstes sofern Pause / vor Unterrichtsbeginn
 	 * 
-	 * @return
+	 * @return Nummer der aktuellen Stunde
 	 */
 	private int getCurrentPeriod() {
 		Calendar today = new GregorianCalendar();
-		//today = new GregorianCalendar(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH), 16, 25);
 		
 		int currentPeriod = 0;
 		int i = 1;
 		for (Calendar currentStartTime : startTimes) {
+			// Ende der Stunde auf Start + 45 Minuten setzen
 			Calendar endTime = (Calendar) currentStartTime.clone();
 			endTime.add(Calendar.MINUTE, 45);
 			
-			if (((today.after(currentStartTime) && today.before(endTime)) || (today.equals(currentStartTime) && today.before(endTime))) || today.before(currentStartTime)) {
+			// aktuelle Stunde ist wenn: Uhrzeit zwischen Stundenbeginn und Stundenende oder Uhrzeit gleich Stundenbeginn
+			// sollte beides nicht eintreffen ist es die aktuelle Stunde falls Uhrzeit vor Stundenbeginn (Pause oder vor 1. Stunde)
+			if (((today.after(currentStartTime) && today.before(endTime)) || today.equals(currentStartTime)) || today.before(currentStartTime)) {
 				currentPeriod = i;
 				break;
 			}
@@ -365,17 +386,18 @@ public class STUPIDActivity extends Activity {
 	}
 	
 	/**
-	 *
+	 * Thread zum asynchronen Aufrufen und Auswerten des Stundenplans
 	 */
 	private class DisplayScheduleTask extends AsyncTask<Void, Void, List<String>> {
 		
 		/**
-		 * 
+		 * Laden und Auswerten des Stundenplans
 		 */
 	     protected List<String> doInBackground(Void... voids) {
 	 		int currentDay = Util.getDayOfWeek();
 	 		int currentWeek = Util.getWeekOfYear();
 	 		
+	 		// laden der Stundenplan Datei
 	 		URL scheduleURL = null;
 	 		try {
 	 			scheduleURL = new URL("http://www.gso-koeln.de/infos/kalender/stupid/lesson.txt");
@@ -384,6 +406,7 @@ public class STUPIDActivity extends Activity {
 	 		}
 	 		List<String[]> completeSchedule = Util.loadDataFromURL(scheduleURL);
 	 		
+	 		// auswerten
 	 		int currentPeriod = getCurrentPeriod();
 			String className = Util.getSchoolClass(getActivity());
 			List<String> lessonInformation = getLessonInformation(currentDay, currentWeek, completeSchedule, currentPeriod, className);
@@ -392,7 +415,7 @@ public class STUPIDActivity extends Activity {
 	     }
 
 	     /**
-	      * 
+	      * Ausgabe des Stundenplans
 	      */
 	     protected void onPostExecute(List<String> result) {
 	    	 printLessonInformation(result, getCurrentPeriod());
